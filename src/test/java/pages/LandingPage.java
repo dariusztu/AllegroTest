c5ef9c0d45c0f4b89119d98f67c9e47c85282963
dariusztu <dariusztu@gmail.com>
2018-07-28 15:55:00 +0200

    Add LandingPageTests class.

--- src/test/java/pages/LandingPage.java
+++ src/test/java/pages/LandingPage.java
@@ -1,14 +1,37 @@
 package pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class LandingPage {
 
-    WebDriver driver;
+    private LandingPage landingPageObject;
+    private WebDriver driver;
 
-    public LandingPage(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
+    @BeforeClass
+    public static void pathSetup() {
+        WebDriverManager.chromedriver().setup();
     }
+
+    @Before
+    public void setupDriver() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void firstSampleTest() {
+        
+    }
+
 }
