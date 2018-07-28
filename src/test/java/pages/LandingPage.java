ffaf17548461deb2dcf76295016c273dddbf8707
dariusztu <dariusztu@gmail.com>
2018-07-28 16:37:14 +0200

    Add LandingPage RODO WebElements.

--- src/test/java/pages/LandingPage.java
+++ src/test/java/pages/LandingPage.java
@@ -6,13 +6,21 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LandingPage {
 
     WebDriver driver;
     private static String url = "https://allegro.pl/";
+
+    @FindBy(css="[data-role=accept-consent][data-analytics-interaction-value=accept]")
+    private WebElement rodoButtonAccept;
+
+    @FindBy(css="[data-role=reject-consent][data-analytics-interaction-value=reject]")
+    private WebElement rodoButtonDeny;
 
     public LandingPage(WebDriver driver) {
         this.driver = driver;
@@ -24,6 +32,14 @@ public class LandingPage {
     public String getPage() {
         driver.get(url);
         return url;
+    }
+
+    public void rodoButtonAcceptClick() {
+        rodoButtonAccept.click();
+    }
+
+    public void rodoButtonDenyClick() {
+        rodoButtonDeny.click();
     }
 
 
