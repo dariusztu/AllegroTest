1bd50259010a196811e3965d9a6d4f79c5e845f0
dariusztu <dariusztu@gmail.com>
2018-09-03 10:26:46 +0200

    Add superclass constructor invoke.

--- src/test/java/pages/LandingPage.java
+++ src/test/java/pages/LandingPage.java
@@ -17,6 +17,7 @@ public class LandingPage    extends BasePage {
     private static String url = "https://allegro.pl/";
 
     public LandingPage(WebDriver driver) {
+        super(driver);
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
