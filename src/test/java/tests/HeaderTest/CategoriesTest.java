b18be18b638d0017fd8c831ecfb9f273fcd04b59
dariusztu <dariusztu@gmail.com>
2018-09-03 18:53:20 +0200

    Add CategoriesTest.

---
+++ src/test/java/tests/HeaderTest/CategoriesTest.java
@@ -0,0 +1,29 @@
+package tests.HeaderTest;
+
+import org.junit.Test;
+import pages.Header.HeaderPage;
+import pages.Header.SearchSection.SearchPage;
+import pages.LandingPage;
+import tests.BaseTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CategoriesTest extends BaseTest {
+
+    private LandingPage landingPageObject;
+    private HeaderPage headerPage;
+
+    @Test
+    public void passIfGoToWystawPrzedmiotIsSuccessfull(){
+        landingPageObject = new LandingPage(driver);
+        headerPage = new HeaderPage(driver);
+        landingPageObject.
+                goTopage().
+                rodoButtonAcceptClick();
+
+        headerPage.categoriesPage.
+                kategorieMainButtonClick().
+                wszystkiekategorieClick();
+    }
+
+}
