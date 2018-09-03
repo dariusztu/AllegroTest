95cae3854b9d5f44d313b29a408b8f6a7d1ceff1
dariusztu <dariusztu@gmail.com>
2018-09-03 18:38:23 +0200

    Add SubCategory1Page

---
+++ src/test/java/pages/Header/CategoriesSection/SubCategory1/SubCategory1Page.java
@@ -0,0 +1,218 @@
+package pages.Header.CategoriesSection.SubCategory1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import pages.BasePage;
+
+    public class SubCategory1Page extends BasePage {
+
+        WebDriver driver;
+
+        public SubCategory1Page(WebDriver driver) {
+            super(driver);
+            this.driver = driver;
+            PageFactory.initElements(driver, this); }
+
+            // elektronika subcategory
+
+            @FindBy(css = "a[title=\"Telewizory\"]")
+            private WebElement elektronikaPodkategoriaTelewizory;
+
+            @FindBy(css = "a[title=\"Fotografia\"]")
+            private WebElement elektronikaPodkategoriaFotografia;
+
+             
+
+            @FindBy(css = "a[title=\"RTV i AGD\"]")
+            private WebElement elektronikaPodkategoriaRTViAGD;
+
+            @FindBy(css = "a[title=\"Laptopy\"]")
+            private WebElement elektronikaPodkategoriaLaptopy;
+
+             
+
+            @FindBy(css = "a[title=\"Komputery\"]")
+            private WebElement elektronikaPodkategoriaKomputery;
+
+            @FindBy(css = "a[title=\"Konsole i Automaty\"]")
+            private WebElement elektronikaPodkategoriaKonsoleiAutomaty;
+
+             
+
+            @FindBy(css = "a[title=\"Telefony i Akcesoria\"]")
+            private WebElement elektronikaPodkategoriaTelefonyiAkcesoria;
+
+            @FindBy(css = "a[title=\"Smartfony\"]")
+            private WebElement elektronikaPodkategoriaSmartfony;
+
+             
+
+            @FindBy(css = "a[title=\"Aparaty cyfrowe\"]")
+            private WebElement elektronikaPodkategoriaAparatyCyfrowe;
+
+            @FindBy(css = "a[title=\"Sprzęt estradowy, studyjny i DJ-ski\"]")
+            private WebElement elektronikaPodkategoriaSprzetEstradowyStudyjny;
+
+             
+
+            @FindBy(css = "a[title=\"Karty graficzne\"]")
+            private WebElement elektronikaPodkategoriaKartyGraficzne;
+
+            @FindBy(css = "a[title=\"Monitory\"]")
+            private WebElement elektronikaPodkategoriaMonitory;
+
+             
+
+            @FindBy(css = "a[title=\"Drukarki i skanery\"]")
+            private WebElement elektronikaPodkategoriaDrukarkiiSkanery;
+
+            @FindBy(css = "a[title=\"iPhone X\"]")
+            private WebElement elektronikaPodkategoriaiPhoneX;
+
+            
+
+            @FindBy(css = "a[title=\"Samsung Galaxy S8\"]")
+            private WebElement elektronikaPodkategoriaSamsungGalaxyS8;
+
+            @FindBy(css = "a[title=\"Huawei P20\"]")
+            private WebElement elektronikaPodkategoriaHuaweiP20;
+
+           
+
+            @FindBy(css = "a[title=\"Ekspresy do kawy\"]")
+            private WebElement elektronikaPodkategoriaEkspresyDoKawy;
+
+            @FindBy(css = "a[title=\"Odkurzacze\"]")
+            private WebElement elektronikaPodkategoriaOdkurzacze;
+
+            
+
+            @FindBy(css = "a[title=\"Lodowki\"]")
+            private WebElement elektronikaPodkategoriaLodowki;
+
+            @FindBy(css = "a[title=\"Plyty grzewcze\"]")
+            private WebElement elektronikaPodkategoriaPlytyGrzewcze;
+
+            
+
+            @FindBy(css = "a[title=\"Samsung Galaxy Note 9\"]")
+            private WebElement elektronikaPodkategoriaSamsungGalaxyNote9;
+
+            @FindBy(css = "a[title=\"Pralki\"]")
+            private WebElement elektronikaPodkategoriaPralki;
+
+
+        public SubCategory1Page elektronikaPodkategoriaTelewizoryClick() {
+            elektronikaPodkategoriaTelewizory.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaFotografiaClick() {
+             elektronikaPodkategoriaFotografia.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaRTViAGDClick() {
+            elektronikaPodkategoriaRTViAGD.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaLaptopyClick() {
+             elektronikaPodkategoriaLaptopy.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaKomputeryClick() {
+             elektronikaPodkategoriaKomputery.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaKonsoleiAutomatyClick() {
+             elektronikaPodkategoriaKonsoleiAutomaty.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaTelefonyiAkcesoriaClick() {
+            elektronikaPodkategoriaTelefonyiAkcesoria.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaSmartfonyClick() {
+             elektronikaPodkategoriaSmartfony.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaAparatyCyfroweClick() {
+             elektronikaPodkategoriaAparatyCyfrowe.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaSprzetEstradowyStudyjnyClick() {
+             elektronikaPodkategoriaSprzetEstradowyStudyjny.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaKartyGraficzneClick() {
+             elektronikaPodkategoriaKartyGraficzne.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaMonitoryClick() {
+             elektronikaPodkategoriaMonitory.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaDrukarkiiSkaneryClick() {
+             elektronikaPodkategoriaDrukarkiiSkanery.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaiPhoneXClick() {
+             elektronikaPodkategoriaiPhoneX.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaSamsungGalaxyS8Click() {
+             elektronikaPodkategoriaSamsungGalaxyS8.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaHuaweiP20Click() {
+             elektronikaPodkategoriaHuaweiP20.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaEkspresyDoKawyClick() {
+             elektronikaPodkategoriaEkspresyDoKawy.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaOdkurzaczeClick() {
+             elektronikaPodkategoriaOdkurzacze.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaLodowkiClick() {
+             elektronikaPodkategoriaLodowki.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaPlytyGrzewczeClick() {
+             elektronikaPodkategoriaPlytyGrzewcze.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaSamsungGalaxyNote9Click() {
+             elektronikaPodkategoriaSamsungGalaxyNote9.click();
+            return this;
+        }
+
+        public SubCategory1Page elektronikaPodkategoriaPralkiClick() {
+             elektronikaPodkategoriaPralki.click();
+            return this.click();
+        }
+    }
+    }
+
