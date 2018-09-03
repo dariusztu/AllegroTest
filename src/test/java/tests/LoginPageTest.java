f2ed21d5c39203662fd4ef6a25887bf14e2d923e
dariusztu <dariusztu@gmail.com>
2018-09-03 10:52:22 +0200

    Change LoginPageTests name to LoginPageTest

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -12,7 +12,7 @@ import pages.LoginPage;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class LoginPageTests {
+public class LoginPageTest {
 
     private LandingPage landingPageObject;
     private LoginPage loginPageObject;
