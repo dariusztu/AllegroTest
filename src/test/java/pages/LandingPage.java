245a406722fbcfa6307229f0c2831fbea71f9741
dariusztu <dariusztu@gmail.com>
2018-09-03 09:58:12 +0200

    Add BasePage heritance.

--- src/test/java/pages/LandingPage.java
+++ src/test/java/pages/LandingPage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LandingPage {
+public class LandingPage    extends BasePage {
 
     WebDriver driver;
     private static String url = "https://allegro.pl/";
