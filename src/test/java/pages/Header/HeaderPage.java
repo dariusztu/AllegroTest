add8da9ae81aba9ceb3b9205f980e0d612d56ca6
dariusztu <dariusztu@gmail.com>
2018-09-03 16:17:40 +0200

    Add selectors.

--- src/test/java/pages/Header/HeaderPage.java
+++ src/test/java/pages/Header/HeaderPage.java
@@ -1,6 +1,8 @@
 package pages.Header;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import pages.BasePage;
 import pages.Header.DropdownSection.DropdownPage;
@@ -19,4 +21,10 @@ public class HeaderPage extends BasePage {
         dropdownPage = new DropdownPage(driver);
         searchPage = new SearchPage(driver);
     }
+
+    @FindBy(css = ".fee54_1Uwxm.fee54_3KwDM > a")
+    private WebElement starButtonObserwowaneOfety;
+
+    @FindBy(css = ".fee54_1Njt4 > div:nth-child(2) > div > div > a")
+    private WebElement koszyk;
 }
