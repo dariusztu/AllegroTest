213a21cace82f783ce9aeda6631401ef679cbc7b
dariusztu <dariusztu@gmail.com>
2018-09-05 17:34:22 +0200

    Add passIfGoToEveryCategoryInZdrowieIsSuccessfull,  passIfGoToEveryMainCategoryIsSuccessfull tests

--- src/test/java/tests/HeaderTest/CategoriesTest.java
+++ src/test/java/tests/HeaderTest/CategoriesTest.java
@@ -1,11 +1,17 @@
 package tests.HeaderTest;
 
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.Header.CategoriesSection.CategoriesPage;
 import pages.Header.HeaderPage;
 import pages.Header.SearchSection.SearchPage;
 import pages.LandingPage;
+import pages.ProductsList.ProductsListBasePage;
 import tests.BaseTest;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +20,7 @@ public class CategoriesTest extends BaseTest {
 
     private LandingPage landingPageObject;
     private HeaderPage headerPage;
+    private ProductsListBasePage productsListBasePage;
 
     @Test
     public void passIfShowCategoryAfterMouseHoverIsSuccessfull(){
@@ -38,4 +45,47 @@ public class CategoriesTest extends BaseTest {
 
     }
 
+    @Test
+    public void passIfGoToEveryCategoryInZdrowieIsSuccessfull() {
+        landingPageObject = new LandingPage(driver);
+        headerPage = new HeaderPage(driver);
+        productsListBasePage = new ProductsListBasePage(driver);
+
+
+        landingPageObject.
+                goTopage().
+                rodoButtonAcceptClick();
+
+        headerPage.
+                categoriesPage.
+                kategorieMainButtonClick().
+                mouseHoverOverCategory(headerPage.categoriesPage.getKategoriaZdrowie());
+
+        headerPage.categoriesPage.subCategory7Page.zdrowiePodkategoriaAparatySluchoweClick();
+        assertThat(productsListBasePage.productsListCategoryTitleGetText()).isEqualTo("Aparaty słuchowe");
+
+
+    }
+
+    @Test public void passIfGoToEveryMainCategoryIsSuccessfull() {
+        landingPageObject = new LandingPage(driver);
+        headerPage = new HeaderPage(driver);
+        productsListBasePage = new ProductsListBasePage(driver);
+
+        landingPageObject.
+                goTopage().
+                rodoButtonAcceptClick();
+
+
+        List<WebElement> allWebElements = headerPage.categoriesPage.CategoriesPageList();
+        for (WebElement lista: allWebElements) { // Go to menu, click category, back to main page, repeat on every category
+            headerPage.categoriesPage.kategorieMainButtonClick();
+            headerPage.categoriesPage.categoryClick(lista);
+            landingPageObject.goTopage();
+
+            //assertThat(productsListBasePage.productsListCategoryTitleGetText()).isEqualTo();
+
+        }
+
+    }
 }
