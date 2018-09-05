a08668c1ed5be2307b370acc9bebc7caf441f979
dariusztu <dariusztu@gmail.com>
2018-09-05 16:50:08 +0200

    Change setProductsListCategoryTitleGetText to  productsListCategoryTitleGetText

--- src/test/java/pages/ProductsList/ProductsListBasePage.java
+++ src/test/java/pages/ProductsList/ProductsListBasePage.java
@@ -1,16 +1,18 @@
 package pages.ProductsList;
+import  pages.BasePage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class ProductsListBasePage {
+public class ProductsListBasePage  extends BasePage {
 
     WebDriver driver;
     private static String url = "https://allegro.pl/";
 
     public ProductsListBasePage(WebDriver driver) {
+        super(driver);
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
@@ -18,7 +20,11 @@ public class ProductsListBasePage {
     @FindBy(css = ".listing-title__category")
     private WebElement productsListCategoryTitle;
 
-    public String setProductsListCategoryTitleGetText() {
+    public WebElement getProductsListCategoryTitle() {
+        return productsListCategoryTitle;
+    }
+
+    public String productsListCategoryTitleGetText() {
         String CategoryTitle = productsListCategoryTitle.getText();
         return CategoryTitle;
     }
