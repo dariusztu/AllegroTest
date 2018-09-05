9f25aa408ece47d77bf75e9c682118d51f12a262
dariusztu <dariusztu@gmail.com>
2018-09-05 11:31:56 +0200

    Add SubCategory9Page

---
+++ src/test/java/pages/Header/CategoriesSection/Subcategory9/SubCategory9Page.java
@@ -0,0 +1,35 @@
+package pages.Header.CategoriesSection.SubCategory9;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import pages.BasePage;
+
+public class SubCategory9Page extends BasePage {
+
+    WebDriver driver;
+
+    public SubCategory9Page(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    // Sport i turystyka subcategory
+
+    
+
+    @FindBy(css = "Wakacje\"]")
+    private WebElement sportiTurystykaPodkategoriaWakacje;
+
+    public WebElement getsportiTurystykaPodkategoriaWakacje() {
+        return sportiTurystykaPodkategoriaWakacje;
+    }
+
+    public SubCategory9Page sportiTurystykaPodkategoriaWakacjeClick() {
+        sportiTurystykaPodkategoriaWakacje.click();
+        return this;
+    }
+
+}
